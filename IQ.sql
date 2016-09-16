ee7b78a073c21e83cfe6a2c1b320d645174e32dd
Paul Jones'git config --global user.email pjones@calmapit.comgit config --global user.name 'Paul Jones <pjones@calmapit.com>
2016-09-16 17:58:13 +1000

    master sql

--- IQ.sql
+++ IQ.sql
@@ -1,1 +1,35 @@
-Select IM_STOCK From IM Where IM_STOCK LIKE 'RT45%'
+Select DESPDATE,
+       ORDERNUM,
+       DESPNOTE,
+       CUSTOMER,
+       ATTENTIONTO,
+       ADDRESS,
+       ADDRESS2,
+       SUBURB,
+       STATE,
+       POSTCODE,
+       FEETYPE,
+       ITEM,
+       DESCRIPTION,
+       QTY,
+       SELLEXCL,
+       LAG(SELLEXCL, 1, 0) OVER (ORDER BY SELLEXCL) AS SDSELL_prev,
+       LEAD(SELLEXCL, 1, 0) OVER (ORDER BY SELLEXCL) AS "Handling Fee",
+       LEAD(SELLEXCL, 2, 0) OVER (ORDER BY SELLEXCL) AS "Line Fee",
+--       CASE   WHEN FEETYPE like 'Freight Fee' OR FEETYPE like 'Manual Freight Fee' AND LAG(DESPNOTE, 1, 0) OVER (ORDER BY DESPNOTE) != DESPNOTE THEN SELLEXCL
+--              ELSE NULL
+--              END AS "Freight Charge",
+       CASE   WHEN FEETYPE like 'Stock' AND LAG(DESPNOTE, 1, 0) OVER (ORDER BY DESPNOTE) != DESPNOTE AND LAG(FEETYPE, 1, 0) OVER (ORDER BY FEETYPE) = 'Pick Fee'  THEN LAG(SELLEXCL, 1, 0) OVER (ORDER BY SELLEXCL)
+              ELSE NULL
+              END AS "Line Charge"
+--       CASE   WHEN FEETYPE like 'Handeling Fee is ' AND LAG(DESPNOTE, 1, 0) OVER (ORDER BY DESPNOTE) != DESPNOTE  THEN SELLEXCL
+--              ELSE NULL
+--              END AS "Order Despatch Charge",
+--       CASE   WHEN FEETYPE like 'Pick Fee ' AND LAG(DESPNOTE, 1, 0) OVER (ORDER BY DESPNOTE) != DESPNOTE  THEN SELLEXCL
+--              ELSE NULL
+--              END AS "Line Charge" 
+
+From TMP_ALL_FEES_F
+Where ORDERNUM = '   1969424' AND DESPNOTE = '   2287094';
+--AND FEETYPE = 'Stock'
+--Order By FEETYPE;
--- Master QSL Scripts.sql
+++ Master QSL Scripts.sql
@@ -4567,10 +4567,57 @@ BEGIN
 END;
 /
 SHOW ERRORS;
-
+dddd
 
 Select SD_STOCK, SH_SPARE_STR_5,SH_CUST,SH_NOTE_1,SH_CUST_REF,SH_NOTE_2, SH_ADDRESS,SH_CITY,SH_ADD_DATE  From SD,SH 
 Where SD_ORDER = SH_ORDER
 AND SD_ORDER in (1982799,
 1982449,
 1982437,
+
+
+
+
+Select f1.DESPDATE,
+       f1.ORDERNUM,
+       f1.DESPNOTE,
+       f1.CUSTOMER,
+       f1.ATTENTIONTO,
+       f1.ADDRESS,
+       f1.ADDRESS2,
+       f1.SUBURB,
+       f1.STATE,
+       f1.POSTCODE,
+       --f1.FEETYPE,
+       f1.ITEM,
+       f1.DESCRIPTION,
+       f1.QTY,
+--       f1.SELLEXCL,
+       --LAG(SELLEXCL, 1, 0) OVER (ORDER BY SELLEXCL) AS SDSELL_prev,
+       --LEAD(SELLEXCL, 1, 0) OVER (ORDER BY SELLEXCL) AS "Handling Fee",
+       --LEAD(SELLEXCL, 2, 0) OVER (ORDER BY SELLEXCL) AS "Line Fee",
+--       CASE   WHEN FEETYPE like 'Freight Fee' OR FEETYPE like 'Manual Freight Fee' AND LAG(DESPNOTE, 1, 0) OVER (ORDER BY DESPNOTE) != DESPNOTE THEN SELLEXCL
+--              ELSE NULL
+--              END AS "Freight Charge",
+       CASE   WHEN f1.FEETYPE like 'Stock' AND LAG(f1.DESPNOTE, 1, 0) OVER (ORDER BY f1.DESPNOTE) != f1.DESPNOTE THEN (Select f2.SELLEXCL From TMP_ALL_FEES_F f2 Where f2.ORDERNUM = f1.ORDERNUM AND f2.FEETYPE = 'Pick Fee' ) --As "Line Charge"-- AND LAG(FEETYPE, 1, 0) OVER (ORDER BY FEETYPE) = 'Pick Fee'  THEN LEAD(SELLEXCL, 2, 0) OVER (ORDER BY SELLEXCL)
+              ELSE 0
+              END AS "Line Charge",
+      CASE   WHEN f1.FEETYPE like 'Stock' AND LAG(f1.DESPNOTE, 1, 0) OVER (ORDER BY f1.DESPNOTE) != f1.DESPNOTE THEN (Select f2.SELLEXCL From TMP_ALL_FEES_F f2 Where f2.ORDERNUM = f1.ORDERNUM AND f2.FEETYPE = 'Handeling Fee is ' ) --As "Line Charge"-- AND LAG(FEETYPE, 1, 0) OVER (ORDER BY FEETYPE) = 'Pick Fee'  THEN LEAD(SELLEXCL, 2, 0) OVER (ORDER BY SELLEXCL)
+              ELSE 0
+              END AS "Order Despatch Charge",
+      CASE   WHEN f1.FEETYPE like 'Stock' AND LAG(f1.DESPNOTE, 1, 0) OVER (ORDER BY f1.DESPNOTE) != f1.DESPNOTE THEN (Select f2.SELLEXCL From TMP_ALL_FEES_F f2 Where f2.ORDERNUM = f1.ORDERNUM AND (f2.FEETYPE like 'Freight Fee' OR f2.FEETYPE like 'Manual Freight Fee') ) --As "Line Charge"-- AND LAG(FEETYPE, 1, 0) OVER (ORDER BY FEETYPE) = 'Pick Fee'  THEN LEAD(SELLEXCL, 2, 0) OVER (ORDER BY SELLEXCL)
+              ELSE 0
+              END AS "Freight Charge"
+--       CASE   WHEN FEETYPE like 'Handeling Fee is ' AND LAG(DESPNOTE, 1, 0) OVER (ORDER BY DESPNOTE) != DESPNOTE  THEN SELLEXCL
+--              ELSE NULL
+--              END AS "Order Despatch Charge",
+--       CASE   WHEN FEETYPE like 'Pick Fee ' AND LAG(DESPNOTE, 1, 0) OVER (ORDER BY DESPNOTE) != DESPNOTE  THEN SELLEXCL
+--              ELSE NULL
+--              END AS "Line Charge" 
+
+From TMP_ALL_FEES_F f1
+Where 
+--f1.ORDERNUM = '   1969424' AND f1.DESPNOTE = '   2287094'
+--AND 
+f1.FEETYPE = 'Stock';
+--Order By FEETYPE;
