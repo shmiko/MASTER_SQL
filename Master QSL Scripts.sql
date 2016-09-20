ea0e21eb023fa28e345438698142b69bc1d0c17d
Paul Jones'git config --global user.email pjones@calmapit.comgit config --global user.name 'Paul Jones <pjones@calmapit.com>
2016-09-20 17:15:12 +1000

    superpartners query

--- Master QSL Scripts.sql
+++ Master QSL Scripts.sql
@@ -4579,33 +4579,33 @@ AND SD_ORDER in (1982799,
 
 --Superpartners Query for EOM
 Select f1.DESPDATE,
-       f1.ORDERNUM,
-       f1.DESPNOTE,
-       f1.CUSTOMER,
-       f1.ATTENTIONTO,
-       f1.ADDRESS,
-       f1.ADDRESS2,
-       f1.SUBURB,
-       f1.STATE,
-       f1.POSTCODE,
-       f1.ITEM,
-       f1.DESCRIPTION,
-       f1.QTY,
-       CASE   WHEN f1.FEETYPE like 'Stock' AND LAG(f1.DESPNOTE, 1, 0) OVER (ORDER BY f1.DESPNOTE) != f1.DESPNOTE THEN (Select f2.SELLEXCL From TMP_ALL_FEES_F f2 Where f2.ORDERNUM = f1.ORDERNUM AND f2.FEETYPE = 'Pick Fee' ) --As "Line Charge"-- AND LAG(FEETYPE, 1, 0) OVER (ORDER BY FEETYPE) = 'Pick Fee'  THEN LEAD(SELLEXCL, 2, 0) OVER (ORDER BY SELLEXCL)
-              ELSE 0
-              END AS "Line Charge",
-      CASE   WHEN f1.FEETYPE like 'Stock' AND LAG(f1.DESPNOTE, 1, 0) OVER (ORDER BY f1.DESPNOTE) != f1.DESPNOTE THEN (Select f2.SELLEXCL From TMP_ALL_FEES_F f2 Where f2.ORDERNUM = f1.ORDERNUM AND f2.FEETYPE = 'Handeling Fee is ' ) --As "Line Charge"-- AND LAG(FEETYPE, 1, 0) OVER (ORDER BY FEETYPE) = 'Pick Fee'  THEN LEAD(SELLEXCL, 2, 0) OVER (ORDER BY SELLEXCL)
-              ELSE 0
-              END AS "Order Despatch Charge",
-      CASE   WHEN f1.FEETYPE like 'Stock' AND LAG(f1.DESPNOTE, 1, 0) OVER (ORDER BY f1.DESPNOTE) != f1.DESPNOTE THEN (Select f2.SELLEXCL From TMP_ALL_FEES_F f2 Where f2.ORDERNUM = f1.ORDERNUM AND (f2.FEETYPE like 'Freight Fee' OR f2.FEETYPE like 'Manual Freight Fee') ) --As "Line Charge"-- AND LAG(FEETYPE, 1, 0) OVER (ORDER BY FEETYPE) = 'Pick Fee'  THEN LEAD(SELLEXCL, 2, 0) OVER (ORDER BY SELLEXCL)
-              ELSE 0
-              END AS "Freight Charge"
- --Daily Van Freight        
+             f1.ORDERNUM,
+             f1.DESPNOTE,
+             f1.CUSTOMER,
+             f1.ATTENTIONTO,
+             f1.ADDRESS,
+             f1.ADDRESS2,
+             f1.SUBURB,
+             f1.STATE,
+             f1.POSTCODE,
+             --f1.FEETYPE,
+             f1.ITEM,
+             f1.DESCRIPTION,
+             f1.QTY,
+             CASE   WHEN f1.FEETYPE like 'Stock' AND LAG(f1.DESPNOTE, 1, 0) OVER (ORDER BY f1.DESPNOTE) != f1.DESPNOTE THEN (Select f2.SELLEXCL From TMP_ALL_FEES_F f2 Where f2.ORDERNUM = f1.ORDERNUM AND f2.FEETYPE = 'Pick Fee' ) --As "Line Charge"-- AND LAG(FEETYPE, 1, 0) OVER (ORDER BY FEETYPE) = 'Pick Fee'  THEN LEAD(SELLEXCL, 2, 0) OVER (ORDER BY SELLEXCL)
+                    ELSE 0
+                    END AS "Line Charge",
+            CASE   WHEN f1.FEETYPE like 'Stock' AND LAG(f1.DESPNOTE, 1, 0) OVER (ORDER BY f1.DESPNOTE) != f1.DESPNOTE THEN (Select f2.SELLEXCL From TMP_ALL_FEES_F f2 Where f2.ORDERNUM = f1.ORDERNUM AND f2.FEETYPE = 'Handeling Fee is ' ) --As "Line Charge"-- AND LAG(FEETYPE, 1, 0) OVER (ORDER BY FEETYPE) = 'Pick Fee'  THEN LEAD(SELLEXCL, 2, 0) OVER (ORDER BY SELLEXCL)
+                    ELSE 0
+                    END AS "Order Despatch Charge",
             CASE   WHEN f1.FEETYPE like 'Stock' AND LAG(f1.DESPNOTE, 1, 0) OVER (ORDER BY f1.DESPNOTE) != f1.DESPNOTE THEN (Select f2.SELLEXCL From TMP_ALL_FEES_F f2 Where f2.ORDERNUM = f1.ORDERNUM AND (f2.FEETYPE like 'Freight Fee' OR f2.FEETYPE like 'Manual Freight Fee') ) --As "Line Charge"-- AND LAG(FEETYPE, 1, 0) OVER (ORDER BY FEETYPE) = 'Pick Fee'  THEN LEAD(SELLEXCL, 2, 0) OVER (ORDER BY SELLEXCL)
+                    ELSE 0
+                    END AS "Freight Charge",
+            --Daily Van Freight        
+            CASE   WHEN (Select Count(*) From TMP_ALL_FEES_F Where ((ADDRESS LIKE '%Casselden%' Or ADDRESS LIKE '%Lonsdale%') OR (ADDRESS2 LIKE '%Casselden%' Or ADDRESS2 LIKE '%Lonsdale%'))) THEN '30.71'
                     ELSE 0
                     END AS "Daily Flat Rate Freight Charge"
       From TMP_ALL_FEES_F f1
       Where f1.FEETYPE = 'Stock' 
-      AND (SH_ADDRESS NOT LIKE '%Casselden%' Or SH_ADDRESS NOT LIKE '%Lonsdale%'
-      OR SH_SUBURB NOT LIKE '%Casselden%' Or SH_SUBURB NOT LIKE '%Lonsdale%')
-      }';    
+      AND ((ADDRESS NOT LIKE '%Casselden%' Or ADDRESS NOT LIKE '%Lonsdale%')
+      OR (ADDRESS2 NOT LIKE '%Casselden%' Or ADDRESS2 NOT LIKE '%Lonsdale%'));
