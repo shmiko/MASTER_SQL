382ec3127d3923c8ffe735a9b879c2e0997ee13f
Paul Jones'git config --global user.email pjones@calmapit.comgit config --global user.name 'Paul Jones <pjones@calmapit.com>
2016-09-22 14:58:22 +1000

    sup ok query

--- Master QSL Scripts.sql
+++ Master QSL Scripts.sql
@@ -4784,3 +4784,58 @@ TRUNC(sysdate+30, 'MONTH')-1 End_of_the_month,
 TRUNC(sysdate, 'DAY')+1 start_of_the_week,  -- starting Monday
 TRUNC(sysdate+6, 'DAY') end_of_the_week     -- finish Sunday
 from dual;
+
+
+ Select  f1.DESPDATE,f1.ORDERNUM,f1.DESPNOTE,f1.CUSTOMER,
+        f1.ATTENTIONTO,f1.ADDRESS,f1.ADDRESS2,f1.SUBURB,f1.STATE,f1.POSTCODE,
+        f1.ITEM,f1.DESCRIPTION,f1.QTY
+             ,CASE   WHEN f1.FEETYPE like 'Stock' AND LAG(f1.DESPNOTE, 1, 0) OVER (ORDER BY f1.DESPNOTE) != f1.DESPNOTE THEN (Select f2.SELLEXCL From TMP_ALL_FEES_F f2 Where f2.ORDERNUM = f1.ORDERNUM AND f2.FEETYPE = 'Pick Fee' ) --As "Line Charge"-- AND LAG(FEETYPE, 1, 0) OVER (ORDER BY FEETYPE) = 'Pick Fee'  THEN LEAD(SELLEXCL, 2, 0) OVER (ORDER BY SELLEXCL)
+                    ELSE 0
+                    END AS "Line Charge"
+            ,CASE   WHEN f1.FEETYPE like 'Stock' AND LAG(f1.DESPNOTE, 1, 0) OVER (ORDER BY f1.DESPNOTE) != f1.DESPNOTE THEN (Select f2.SELLEXCL From TMP_ALL_FEES_F f2 Where f2.ORDERNUM = f1.ORDERNUM AND f2.FEETYPE = 'Handeling Fee is ' ) --As "Line Charge"-- AND LAG(FEETYPE, 1, 0) OVER (ORDER BY FEETYPE) = 'Pick Fee'  THEN LEAD(SELLEXCL, 2, 0) OVER (ORDER BY SELLEXCL)
+                    ELSE 0
+                    END AS "Order Despatch Charge"
+            ,CASE   WHEN f1.FEETYPE like 'Stock' AND (LAG(f1.DESPNOTE, 1, 0) OVER (ORDER BY f1.DESPNOTE) != f1.DESPNOTE)  AND ((UPPER(ADDRESS) NOT LIKE '%CASSELDEN%' Or UPPER(ADDRESS) NOT LIKE '2 LONSDALE%') OR (UPPER(ADDRESS2) NOT LIKE '%CASSELDEN%' Or UPPER(ADDRESS2) NOT LIKE '2 LONSDALE%'))
+                    THEN (
+                          Select f2.SELLEXCL From TMP_ALL_FEES_F f2 Where f2.ORDERNUM = f1.ORDERNUM AND (f2.FEETYPE like 'Freight Fee' OR f2.FEETYPE like 'Manual Freight Fee') AND ROWNUM = 1 AND ((UPPER(ADDRESS) NOT LIKE '%CASSELDEN%' Or UPPER(ADDRESS) NOT LIKE '2 LONSDALE%') OR (UPPER(ADDRESS2) NOT LIKE '%CASSELDEN%' Or UPPER(ADDRESS2) NOT LIKE '2 LONSDALE%')) --As "Line Charge"-- AND LAG(FEETYPE, 1, 0) OVER (ORDER BY FEETYPE) = 'Pick Fee'  THEN LEAD(SELLEXCL, 2, 0) OVER (ORDER BY SELLEXCL)
+                    ELSE 0
+                    END AS "Freight Charge"
+      From TMP_ALL_FEES_F f1
+	  
+	  
+	  
+	  Select  f1.DESPDATE,f1.ORDERNUM,f1.DESPNOTE,f1.CUSTOMER,
+        f1.ATTENTIONTO,Upper(f1.ADDRESS),Upper(f1.ADDRESS2),f1.SUBURB,f1.STATE,f1.POSTCODE,
+        f1.ITEM,f1.DESCRIPTION,f1.QTY
+             ,CASE    WHEN f1.FEETYPE like 'Stock' AND LAG(f1.DESPNOTE, 1, 0) OVER (ORDER BY f1.DESPNOTE) != f1.DESPNOTE THEN (Select f2.SELLEXCL From TMP_ALL_FEES_F f2 Where f2.ORDERNUM = f1.ORDERNUM AND f2.FEETYPE = 'Pick Fee' ) --As "Line Charge"-- AND LAG(FEETYPE, 1, 0) OVER (ORDER BY FEETYPE) = 'Pick Fee'  THEN LEAD(SELLEXCL, 2, 0) OVER (ORDER BY SELLEXCL)
+                      ELSE 0
+                      END AS "Line Charge"
+            ,CASE     WHEN f1.FEETYPE like 'Stock' AND LAG(f1.DESPNOTE, 1, 0) OVER (ORDER BY f1.DESPNOTE) != f1.DESPNOTE 
+                      THEN (
+                            Select f2.SELLEXCL 
+                            From TMP_ALL_FEES_F f2 
+                            Where f2.ORDERNUM = f1.ORDERNUM 
+                            AND f2.FEETYPE = 'Handeling Fee is ' 
+                            ) --As "Line Charge"-- AND LAG(FEETYPE, 1, 0) OVER (ORDER BY FEETYPE) = 'Pick Fee'  THEN LEAD(SELLEXCL, 2, 0) OVER (ORDER BY SELLEXCL)
+                      ELSE 0
+                      END AS "Order Despatch Charge"
+            ,CASE     WHEN f1.FEETYPE like 'Stock' AND (LAG(f1.DESPNOTE, 1, 0) OVER (ORDER BY f1.DESPNOTE) != f1.DESPNOTE)
+                           AND InStr(UPPER(ADDRESS),'CASSELDEN') < 1 
+                            AND InStr(UPPER(ADDRESS2),'2 LONSDALE') < 1 
+                             --OR (InStr(UPPER(ADDRESS),'%CASSELDEN%') < 1 
+                            --AND InStr(UPPER(ADDRESS2),'%2 LONSDALE%') < 1
+                            --)
+                            --AND ((UPPER(ADDRESS) NOT LIKE '%CASSELDEN%' Or UPPER(ADDRESS) NOT LIKE '%2 LONSDALE%') OR (UPPER(ADDRESS2) NOT LIKE '%CASSELDEN%' Or UPPER(ADDRESS2) NOT LIKE '%2 LONSDALE%'))
+                      THEN  (
+                          Select f2.SELLEXCL 
+                          From TMP_ALL_FEES_F f2 
+                          Where f2.ORDERNUM = f1.ORDERNUM 
+                          AND (f2.FEETYPE like 'Freight Fee' OR f2.FEETYPE like 'Manual Freight Fee') 
+                          AND ROWNUM = 1 
+                          --AND ((UPPER(ADDRESS) NOT LIKE '%CASSELDEN%' Or UPPER(ADDRESS) NOT LIKE '%2 LONSDALE%') OR (UPPER(ADDRESS2) NOT LIKE '%CASSELDEN%' Or UPPER(ADDRESS2) NOT LIKE '%2 LONSDALE%'))
+                          )
+                      ELSE 0
+                      END AS "Freight Charge"
+      From TMP_ALL_FEES_F f1
+      Where f1.FEETYPE = 'Stock' 
+
