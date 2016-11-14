7cd46718f144eb65dcbc54382d2b40e404951774
Paul Jones'git config --global user.email pjones@calmapit.comgit config --global user.name 'Paul Jones <pjones@calmapit.com>
2016-11-14 17:47:16 +1100

    WMS SOU ALL

--- WMSOU_ALL.sql
+++ WMSOU_ALL.sql
@@ -1,4 +1,7 @@
-SELECT Ord.SO_ID SalesOrder, 
+SELECT 
+'SOH' SOH,
+Ord.SO_ID SalesOrder,
+Ord.CREATED_DATE OrderDate, 
 ISNULL(Cust.[DATAFLEX RECNUM ONE],'') Customer,
 ISNULL(Cust.[AC NO],'') CustomerName,
 ISNULL(Ord.CUST_SO_ID,'') CustomerOrderID, 
@@ -15,7 +18,12 @@ ISNULL(ShipToAddress.CITY,'') ShipToCity,
 ISNULL(ShipToAddress.STATE_CODE,'') ShipToStateCode, 
 ISNULL(ShipToAddress.ZIP_CODE,'') ShipToZipCode, 
 ISNULL(ShipToAddress.CountyName,'') ShipToCountyName, 
-ISNULL(ShipToCountry.COUNTRY_CODE3,'') ShipToCountry
+ISNULL(ShipToCountry.COUNTRY_CODE3,'') ShipToCountry,
+'',
+'' ,
+'' ,
+'' ,
+'' 
 FROM SALES_ORDER Ord 
 INNER JOIN BATCH Inp ON Ord.BATCH_ID = Inp.BATCH_ID 
 INNER JOIN RECIPIENT OrderBy On OrderBy.RECIP_ID = Ord.ORDER_BY_ID 
@@ -25,10 +33,31 @@ INNER JOIN COUNTRY ShipToCountry on ShipToCountry.COUNTRY_ID = ShipToAddress.COU
 INNER JOIN SO_LINE_ITEM Sol ON Ord.SO_ID = SOl.SO_ID 
 INNER JOIN DEBTOR Cust ON Cust.[DATAFLEX RECNUM ONE] = Ord.CUST_ID
 INNER JOIN PAPSIZE Sku ON Sku.[INVENTORY CODE] = Sol.INVENTORY_CODE
+--ORDER BY Ord.CREATED_DATE Desc
 
 UNION 
 
 SELECT 
+'SOL' SOL,
+Sol.[LINE_ITEM_NO] SalesOrderLine,
+Ord.CREATED_DATE OrderDate, 
+'' Customer,
+''  CustomerName,
+''  CustomerOrderID, 
+''  ExtRef,
+''  OrderByCustomerRecipientID, 
+''  OrderByName, 
+''  OrderByCompanyName, 
+''  ShipToName, 
+''  ShipToCompanyName, 
+''  ShipToAddress1, 
+''  ShipToAddress1, 
+''  ShipToAddress1, 
+''  ShipToCity, 
+''  ShipToStateCode, 
+''  ShipToZipCode, 
+''  ShipToCountyName, 
+''  ShipToCountry,
 ISNULL(Sol.INVENTORY_CODE,'') InventoryCode, 
 ISNULL(Sku.[PO UOM DESC],'') UOM,
 ISNULL(Sol.[REVISION_CODE],'') ID,
