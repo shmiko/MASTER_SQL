fa2716b07d9b53fd7a5a7bb78a0f8ae5f08d796b
Paul Jones'git config --global user.email pjones@calmapit.comgit config --global user.name 'Paul Jones <pjones@calmapit.com>
2017-02-01 17:14:29 +1100

    WMS Activity

--- PS_WMS_EOM_ACTIVITY.sql
+++ PS_WMS_EOM_ACTIVITY.sql
@@ -1,16 +1,15 @@
-DECLARE @FromShipDate as date
-DECLARE @ToShipDate as date
-DECLARE @JavelinNumber as varchar(10)
-DECLARE @JavelinLetter as varchar(10)
-DECLARE @OWNumber as VARCHAR(10)
-DECLARE @WMS_OrderNumber as VARCHAR(10)
+DECLARE @FromShipDate as date, @ToShipDate as date, @JavelinNumber as varchar(10), 
+@JavelinLetter as varchar(10), @OWNumber as VARCHAR(10), @WMS_OrderNumber as VARCHAR(10),
+@Datetime1 varchar(19), @DateTime2 varchar(19)
+SET @Datetime1 = CONVERT(VARCHAR, DATEADD(DAY, -33 ,SYSDATETIME()), 121) -- Get System DateTime minus 1 days
+SET @Datetime2 = CONVERT(VARCHAR, DATEADD(DAY, 0 ,SYSDATETIME()), 121)  -- Get System DateTime plus 0 days
 SELECT @FromShipDate = '09/01/2016'   
 SELECT @ToShipDate = '12/16/2016'			--Note:  Make this +1 days from your last ship date
 SET @JavelinLetter = 'W'
 --SET @JavelinNumber = @JavelinLetter + '%' -- Note: Use this to get all Javelin Orders
-SET @OWNumber = 'W1693170'					-- Note: Use this to get a single javelin Order
+SET @OWNumber = 'W1719524'					-- Note: Use this to get a single javelin Order - Tested W1719524
 
-/* Items */
+/* Item */
 SELECT
 	DEBTOR.[DATAFLEX RECNUM ONE]					as ID,
 	DEBTOR.[AC NO]									as Customer,
@@ -27,7 +26,7 @@ SELECT
 	 FROM [LiveData].[dbo].PACKAGE 
 	 WHERE PICK_ID=SO_LINE_ITEM.PICK_ID)			as DespDate, 
 	'Item'											as FeeType, 
-	SO_LINE_ITEM.ITEM_NO							as Item, 
+	CAST(PAPSIZE.[INVENTORY CODE] as nvarchar)						as Item, 
 	SO_LINE_ITEM.ITEM_DESCRIPTION					as Description, 
 	SO_LINE_ITEM.PACK_QTY							as Qty,
 	PAPSIZE.[UNIT OF ISSUE]							as UOI,
@@ -68,64 +67,153 @@ FROM  [LiveData].[dbo].[SO_LINE_ITEM]
 	LEFT JOIN [LiveData].[dbo].PACKAGE				ON PACKAGE.SO_ID				= SALES_ORDER.SO_ID
 	INNER JOIN [LiveData].[dbo].SO_LINE_ITEM_PRICE	ON (SO_LINE_ITEM_PRICE.SO_ID	= SO_LINE_ITEM.SO_ID) 
 		AND (SO_LINE_ITEM_PRICE.LINE_ITEM_NO = SO_LINE_ITEM.LINE_ITEM_NO)
+	LEFT JOIN livedata.dbo.PICK PICKS				ON PICKS.SO_ID					= SALES_ORDER.SO_ID 
 WHERE ISNULL(SO_LINE_ITEM.PICK_ID, '0') >		0 
 	AND ISNULL(SO_LINE_ITEM.ITEM_NO,0)	<>		0 
-	AND (SO_LINE_ITEM.CREATED_DATE		>=		@FromShipDate 
-	AND SO_LINE_ITEM.CREATED_DATE		<=		@ToShipDate)
-	--AND (SALES_ORDER.CUST_SO_ID			LIKE	@JavelinNumber
-	--OR  SALES_ORDER.CUST_SO_ID			=		@OWNumber)
+	--AND (SO_LINE_ITEM.CREATED_DATE		>=		@FromShipDate 
+	--AND SO_LINE_ITEM.CREATED_DATE		<=		@ToShipDate)
+	and (SALES_ORDER.CREATED_DATE > = @Datetime1 and SALES_ORDER.CREATED_DATE <= @Datetime2)
+	AND (SALES_ORDER.CUST_SO_ID			LIKE	@JavelinNumber
+	OR  SALES_ORDER.CUST_SO_ID			LIKE		@OWNumber)
+	and PICKS.PICK_STATUS = '21'
 
 UNION 
 
 /* Pickfees  */
-SELECT 
-	DEBTOR.[DATAFLEX RECNUM ONE]					as ID,
-	DEBTOR.[AC NO]									as Customer,
-	SALES_ORDER.BILL_TO_ID							as CustomerId, 
-	SALES_ORDER.CUST_ID								as ParentId, 
-	DEBTOR.NAMES									as Parent, 
-	''												as CostCentre,
-	SALES_ORDER.SO_ID								as OrderNum, 
-	SALES_ORDER.CUST_SO_ID							as OrderWareNum, 
-	''												as CustRef, 
-	''												as PickSlip,
-	''												as DespNote,
-	FF_TRANS.TIME_START							as DespDate, 
-	'PriceList'									as FeeType, 
-	FF_TRANS.TIMELINE_ID							as Item, 
-	FF_TIMELINE.TIMELINE							as Description, 
-	FF_TRANS.ACT_QTY								as Qty,
-	''												as UOI, 
-	'Each'											as UnitOfIssDesc, 
-	FF_TRANS.ACT_PPU								as UnitPrice, 
-	(FF_TRANS.ACT_PPU * FF_TRANS.ACT_QTY)			as SellExcl, 
-	((FF_TRANS.ACT_PPU * FF_TRANS.ACT_QTY) 
-			+ ISNULL(FF_TRANS.SalesTax,0))			as SellIncl, 
-	''												as DeliverTo, 
-	''												as AttentionTo, 
-	ISNULL(ShipToAddress.ADDR_1,'')					as Address1, 
-	ISNULL(ShipToAddress.ADDR_2,'')					as Address3, 
-	ISNULL(ShipToAddress.ADDR_3,'')					as Address3, 
-	ISNULL(ShipToAddress.CITY,'')					as Suburb, 
-	ISNULL(ShipToAddress.STATE_CODE,'')				as "State", 
-	ISNULL(ShipToAddress.ZIP_CODE,'')				as PostCode, 
-	ISNULL(ShipToCountry.COUNTRY_NAME,'')			as Country,
-	'0'												as Weight, 
-	'0'												as Packages
-FROM [LiveData].[dbo].FF_TRANS
-	INNER JOIN [LiveData].[dbo].SALES_ORDER			ON SALES_ORDER.SO_ID			= FF_TRANS.SO_ID
-	INNER JOIN RECIPIENT							ON RECIPIENT.RECIP_ID			= SALES_ORDER.SHIP_TO_ID
-	INNER JOIN FFADDRESS ShipToAddress				ON ShipToAddress.ADDRESS_ID 	= Recipient.DEF_ADDRESS_ID 
-	INNER JOIN COUNTRY ShipToCountry				ON ShipToCountry.COUNTRY_NUMBER = ShipToAddress.COUNTRY_ID 
-	INNER JOIN [LiveData].[dbo].FF_TIMELINE			ON FF_TIMELINE.TIMELINE_ID		= FF_TRANS.TIMELINE_ID
-	INNER JOIN [LiveData].[dbo].CUSTOMER			ON CUSTOMER.CUST_ID				= SALES_ORDER.CUST_ID
-	INNER JOIN [LiveData].[dbo].DEBTOR				ON DEBTOR.[DATAFLEX RECNUM ONE] = CUSTOMER.DEBTOR_RECNUM
-WHERE ISNULL(FF_TRANS.SO_ID, '0')	>	0 
-	AND (FF_TRANS.TIME_START			>=		@FromShipDate 
-	AND FF_TRANS.TIME_START				<=		@ToShipDate)
-	--AND (SALES_ORDER.CUST_SO_ID			=		@JavelinNumber
-	--OR  SALES_ORDER.CUST_SO_ID			=		@OWNumber)
+SELECT TOP 1
+	DEBTOR.[DATAFLEX RECNUM ONE]													as ID,
+	DEBTOR.[AC NO]																	as Customer,
+	SALES_ORDER.BILL_TO_ID															as CustomerId, 
+	SALES_ORDER.CUST_ID																as ParentId, 
+	DEBTOR.NAMES																	as Parent, 
+	SO_LINE_ITEM.COST_CENTER														as CostCentre,
+	SALES_ORDER.SO_ID																as OrderNum, 
+	SALES_ORDER.CUST_SO_ID															as OrderWareNum, 
+	SO_LINE_ITEM.PO_NO																as CustRef, 
+	PACKS.PICK_ID																	as PickSlip,
+	PACKS.PICK_ID																	as DespNote,
+	Trans.TIME_START																as DespDate, 
+	'Pick Fee'																		as FeeType, 
+	CAST('FEEPICK'	as nvarchar)													as Item, 
+	--''																				as InventoryCode,
+	'Line Picking Fee'																as "Description", 
+	[bsg_support].[dbo].[ufnGetPickLineCount](SALES_ORDER.SO_ID)					as Qty,
+	--[bsg_support].[dbo].[ufnGetPickLineCount](SALES_ORDER.SO_ID)					as OrderQty,
+	'1'																				as UOI, 
+	'Each'																			as UnitOfIssDesc, 
+	Trans.ACT_PPU																	as UnitPrice, 
+	(Trans.ACT_PPU * [bsg_support].[dbo].[ufnGetPickLineCount](SALES_ORDER.SO_ID))	as SellExcl, 
+	((Trans.ACT_PPU * [bsg_support].[dbo].[ufnGetPickLineCount](SALES_ORDER.SO_ID)) *   1.1)														as SellIncl, 
+	--RECIPIENT.CUST_RECIP_ID															as OrderByCustomerRecipientID, 
+	ISNULL(Recipient.COMPANY_NAME,'')												as DeliverTo, 
+	ISNULL((RECIPIENT.FIRST_NAME 
+		+ ' ' 
+		+ RECIPIENT.LAST_NAME),'')													as AttentionTo,
+	ISNULL(ShipToAddress.ADDR_1,'') 												as Address1, 
+	ISNULL(ShipToAddress.ADDR_2,'') 												as Address3, 
+	ISNULL(ShipToAddress.ADDR_3,'') 												as Address3, 
+	ISNULL(ShipToAddress.CITY,'') 													as Suburb, 
+	ISNULL(ShipToAddress.STATE_CODE,'') 											as "State", 
+	ISNULL(ShipToAddress.ZIP_CODE,'')												as PostCode, 
+	ISNULL(ShipToCountry.COUNTRY_NAME,'')											as Country,
+	'0'																				as "Weight", 
+	'0'																				as "Packages"
+	--[SO_LINE_ITEM].INVENTORY_CODE
+FROM  [SO_LINE_ITEM]
+	INNER JOIN SALES_ORDER					ON SALES_ORDER.SO_ID			= SO_LINE_ITEM.SO_ID
+	INNER JOIN CUSTOMER						ON CUSTOMER.CUST_ID				= SO_LINE_ITEM.CUST_ID
+	INNER JOIN DEBTOR						ON DEBTOR.[DATAFLEX RECNUM ONE] = CUSTOMER.DEBTOR_RECNUM
+	INNER JOIN PAPSIZE						ON PAPSIZE.[CODE]				= cast(SO_LINE_ITEM.ITEM_NO as char) 
+	INNER JOIN RECIPIENT					ON RECIPIENT.RECIP_ID			= SO_LINE_ITEM.SHIP_TO_ID
+	INNER JOIN FFADDRESS ShipToAddress		ON ShipToAddress.ADDRESS_ID 	= Recipient.DEF_ADDRESS_ID 
+	INNER JOIN COUNTRY ShipToCountry		ON ShipToCountry.COUNTRY_NUMBER = ShipToAddress.COUNTRY_ID 
+	LEFT JOIN OrderShipTo					ON OrderShipTo.OrderId			= SALES_ORDER.SO_ID
+	LEFT JOIN PACKAGE						ON PACKAGE.SO_ID				= SALES_ORDER.SO_ID
+	INNER JOIN FF_TRANS Trans				ON SALES_ORDER.SO_ID 			= Trans.SO_ID 
+	INNER JOIN FF_TIMELINE Timeline			ON Timeline.TIMELINE_ID 		= Trans.TIMELINE_ID
+	INNER JOIN SO_LINE_ITEM_PRICE			ON (SO_LINE_ITEM_PRICE.SO_ID	= SO_LINE_ITEM.SO_ID) 
+		AND (SO_LINE_ITEM_PRICE.LINE_ITEM_NO = SO_LINE_ITEM.LINE_ITEM_NO)
+	LEFT JOIN livedata.dbo.PICK PICKS		ON PICKS.SO_ID					= SALES_ORDER.SO_ID 
+	LEFT JOIN [LiveData].[dbo].PACKAGE PACKS		ON PACKS.SO_ID				= SALES_ORDER.SO_ID
+WHERE --(SO_LINE_ITEM.CREATED_DATE		>=		@FromShipDate 
+	--AND SO_LINE_ITEM.CREATED_DATE		<=		@ToShipDate)
+	 (SALES_ORDER.CREATED_DATE > = @Datetime1 and SALES_ORDER.CREATED_DATE <= @Datetime2)
+	AND (SALES_ORDER.CUST_SO_ID			LIKE	@JavelinNumber
+	OR  SALES_ORDER.CUST_SO_ID			LIKE		@OWNumber)
+	AND Trans.[LINE_ITEM_NO]			=		[SO_LINE_ITEM].LINE_ITEM_NO
+	AND [SO_LINE_ITEM].INVENTORY_CODE	NOT IN	('EMERQSRFEE') -- add other non stock items in here to exclude
+	and PICKS.PICK_STATUS = '21'
+	AND Trans.ACTIVITY_ID = '17'
 
+	UNION 
+/* handling fees */
+SELECT TOP 1
+	DEBTOR.[DATAFLEX RECNUM ONE]													as ID,
+	DEBTOR.[AC NO]																	as Customer,
+	SALES_ORDER.BILL_TO_ID															as CustomerId, 
+	SALES_ORDER.CUST_ID																as ParentId, 
+	DEBTOR.NAMES																	as Parent, 
+	SALES_ORDER.[COMPANY CODE]														as CostCentre,
+	SALES_ORDER.SO_ID																as OrderNum, 
+	SALES_ORDER.CUST_SO_ID															as OrderWareNum, 
+	SO_LINE_ITEM.PO_NO																as CustRef, 
+	PACKS.PICK_ID																	as PickSlip,
+	PACKS.PICK_ID																	as DespNote,
+	Trans.TIME_START																as DespDate, 
+	'Handeling Fee'																	as FeeType, 
+	CAST('FEEHANDLING'	as nvarchar)												as Item, 
+	--''																				as InventoryCode,
+	'Despatch Handeling Fee'														as "Description", 
+	'1'																				as Qty,
+	--[bsg_support].[dbo].[ufnGetPickLineCount](SALES_ORDER.SO_ID)					as OrderQty,
+	'1'																				as UOI, 
+	'Each'																			as UnitOfIssDesc, 
+	Trans.ACT_PPU																	as UnitPrice, 
+	Trans.ACT_PPU 	as SellExcl, 
+	(Trans.ACT_PPU *  1.1)															as SellIncl, 
+	--RECIPIENT.CUST_RECIP_ID															as OrderByCustomerRecipientID, 
+	ISNULL(Recipient.COMPANY_NAME,'')												as DeliverTo, 
+	ISNULL((RECIPIENT.FIRST_NAME 
+		+ ' ' 
+		+ RECIPIENT.LAST_NAME),'')													as AttentionTo,
+	ISNULL(ShipToAddress.ADDR_1,'') 												as Address1, 
+	ISNULL(ShipToAddress.ADDR_2,'') 												as Address3, 
+	ISNULL(ShipToAddress.ADDR_3,'') 												as Address3, 
+	ISNULL(ShipToAddress.CITY,'') 													as Suburb, 
+	ISNULL(ShipToAddress.STATE_CODE,'') 											as "State", 
+	ISNULL(ShipToAddress.ZIP_CODE,'')												as PostCode, 
+	ISNULL(ShipToCountry.COUNTRY_NAME,'')											as Country,
+	'0'																				as "Weight", 
+	'0'																				as "Packages"
+	--Trans.ACTIVITY_ID
+	--[SO_LINE_ITEM].INVENTORY_CODE
+FROM  --[SO_LINE_ITEM]
+	--INNER JOIN 
+	SALES_ORDER					
+	INNER JOIN SO_LINE_ITEM ON SALES_ORDER.SO_ID = SO_LINE_ITEM.SO_ID
+	INNER JOIN CUSTOMER						ON CUSTOMER.CUST_ID				= SALES_ORDER.CUST_ID
+	INNER JOIN DEBTOR						ON DEBTOR.[DATAFLEX RECNUM ONE] = CUSTOMER.DEBTOR_RECNUM
+	--INNER JOIN PAPSIZE						ON PAPSIZE.[CODE]				= cast(SO_LINE_ITEM.ITEM_NO as char) 
+	INNER JOIN RECIPIENT					ON RECIPIENT.RECIP_ID			= SALES_ORDER.SHIP_TO_ID
+	INNER JOIN FFADDRESS ShipToAddress		ON ShipToAddress.ADDRESS_ID 	= Recipient.DEF_ADDRESS_ID 
+	INNER JOIN COUNTRY ShipToCountry		ON ShipToCountry.COUNTRY_NUMBER = ShipToAddress.COUNTRY_ID 
+	LEFT JOIN OrderShipTo					ON OrderShipTo.OrderId			= SALES_ORDER.SO_ID
+	LEFT JOIN PACKAGE						ON PACKAGE.SO_ID				= SALES_ORDER.SO_ID
+	INNER JOIN FF_TRANS Trans				ON SALES_ORDER.SO_ID 			= Trans.SO_ID 
+	INNER JOIN FF_TIMELINE Timeline			ON Timeline.TIMELINE_ID 		= Trans.TIMELINE_ID
+	--INNER JOIN SO_LINE_ITEM_PRICE			ON (SO_LINE_ITEM_PRICE.SO_ID	= SO_LINE_ITEM.SO_ID) 
+	--	AND (SO_LINE_ITEM_PRICE.LINE_ITEM_NO = SO_LINE_ITEM.LINE_ITEM_NO)
+	LEFT JOIN [LiveData].[dbo].PACKAGE PACKS		ON PACKS.SO_ID				= SALES_ORDER.SO_ID
+	LEFT JOIN livedata.dbo.PICK PICKS		ON PICKS.SO_ID					= SALES_ORDER.SO_ID 
+WHERE --(SO_LINE_ITEM.CREATED_DATE		>=		@FromShipDate 
+	--AND SO_LINE_ITEM.CREATED_DATE		<=		@ToShipDate)
+	-- (SALES_ORDER.CREATED_DATE > = @Datetime1 and SALES_ORDER.CREATED_DATE <= @Datetime2)
+	 (SALES_ORDER.SO_ID	=		'9863')
+	--AND Trans.[LINE_ITEM_NO]			=		[SO_LINE_ITEM].LINE_ITEM_NO
+	--AND [SO_LINE_ITEM].INVENTORY_CODE	NOT IN	('EMERQSRFEE') -- add other non stock items in here to exclude
+	--AND Trans.ACTIVITY_ID LIKE '23'
+	--and PICKS.PICK_STATUS = '21'
+ -- Where SO_ID = '9863'
 UNION
 
 /* Shipping */
@@ -143,17 +231,18 @@ SELECT
 	''												as DespNote, 
 	PACKAGE.SHIP_DATE								as DespDate, 
 	'Shipping'										as FeeType, 
-	''												as Item, 
-	SHIPPING_MODE.SHIP_MODE_DESC					as Description, 
+	cast('COURIER' as nvarchar)												as Item, 
+	'Ship Ref:' + PACKAGE.TRACKING_NO					as Description, 
 	'1'												as Qty,
-	''												as UOI	, 
-	''												as UnitOfIssDesc, 
+	'1'												as UOI	, 
+	'Each'												as UnitOfIssDesc, 
 	SO_CHARGE.AMOUNT								as UnitPrice, 
 	SO_CHARGE.AMOUNT								as SellExcl, 
-	(SO_CHARGE.AMOUNT 
-	+ ISNULL(SO_CHARGE.SALES_TAX,0))				as SellIncl, 
-	''												as DeliverTo, 
-	''												as AttentionTo,  
+	(SO_CHARGE.AMOUNT *  1.1)						as SellIncl, 
+	ISNULL(Recipient.COMPANY_NAME,'')				as DeliverTo, 
+	ISNULL((RECIPIENT.FIRST_NAME 
+		+ ' ' 
+		+ RECIPIENT.LAST_NAME),'')					as AttentionTo,  
 	ISNULL(ShipToAddress.ADDR_1,'')					as Address1, 
 	ISNULL(ShipToAddress.ADDR_2,'')					as Address3, 
 	ISNULL(ShipToAddress.ADDR_3,'')					as Address3, 
@@ -172,10 +261,16 @@ FROM [LiveData].[dbo].SO_CHARGE
 	INNER JOIN [LiveData].[dbo].DEBTOR				ON DEBTOR.[DATAFLEX RECNUM ONE] = CUSTOMER.DEBTOR_RECNUM
 	LEFT JOIN [LiveData].[dbo].PACKAGE				ON PACKAGE.PACKAGE_ID			= SO_CHARGE.PackageID
 	LEFT JOIN [LiveData].[dbo].SHIPPING_MODE		ON SHIPPING_MODE.SHIP_MODE_ID	= PACKAGE.SHIP_MODE_ID
+	LEFT JOIN livedata.dbo.PICK PICKS				ON PICKS.SO_ID					= SO_CHARGE.SO_ID 
 WHERE ISNULL(SO_CHARGE.SO_ID, '0')		>		0 
-	AND (SO_CHARGE.MODIFIED_DATE		>=		@FromShipDate 
-	AND SO_CHARGE.MODIFIED_DATE			<=		@ToShipDate)
-	--AND (SALES_ORDER.CUST_SO_ID			=		@JavelinNumber
-	--OR  SALES_ORDER.CUST_SO_ID			=		@OWNumber)
+	--AND (SO_CHARGE.MODIFIED_DATE		>=		@FromShipDate 
+	--AND SO_CHARGE.MODIFIED_DATE			<=		@ToShipDate)
+	and (SALES_ORDER.CREATED_DATE > = @Datetime1 and SALES_ORDER.CREATED_DATE <= @Datetime2)
+	AND (SALES_ORDER.CUST_SO_ID			LIKE		@JavelinNumber
+	OR  SALES_ORDER.CUST_SO_ID			LIKE		@OWNumber)
+	and PICKS.PICK_STATUS = '21'
 
 ORDER BY SALES_ORDER.SO_ID Desc
+
+
+-- All other activity based core complexities need to be added -- 
